948109b2b6909917fb4b8517e946e0e791b237d0
denisandrejev6-hue <denisandrejev6@gmail.com>
2026-03-24 14:00:17 +0000

    da

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -36,6 +36,12 @@
                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
             </div>
 
+            <div class="mb-6">
+                <label for="password_confirmation" class="block text-gray-700 text-sm font-bold mb-2">Apstipriniet paroli:</label>
+                <input type="password" name="password_confirmation" id="password_confirmation" required
+                       class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+            </div>
+
             <div class="flex items-center justify-between">
                 <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
                     Reģistrēties
