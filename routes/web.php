73afa47cb111bc90a00f2d90a74f6317feeefda8
denisandrejev6-hue <denisandrejev6@gmail.com>
2026-03-24 10:44:23 +0000

    registracija

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@ use App\Http\Controllers\JaunumiController;
 
 Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
+Route::get('/register', [AuthController::class, 'showRegistrationForm'])->name('register');
+Route::post('/register', [AuthController::class, 'register']);
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
 Route::get('/', function () {
