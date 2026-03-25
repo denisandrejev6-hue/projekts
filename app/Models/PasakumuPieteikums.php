eedf231c1f27fe144ada6cdba515942b1204da55
denisandrejev6-hue <denisandrejev6@gmail.com>
2026-03-25 13:34:11 +0000

    dd

--- app/Models/PasakumuPieteikums.php
+++ app/Models/PasakumuPieteikums.php
@@ -4,54 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Pasakumi extends Model
+class PasakumuPieteikums extends Model
 {
-    protected $table = 'pasakumi';
+    protected $table = 'pasakumu_pieteikumi';
     protected $primaryKey = 'ID';
     public $timestamps = false;
 
     protected $fillable = [
-        'nosaukums',
-        'kategorija',
-        'datums_no',
-        'datums_lidz',
-        'sakuma_laiks',
-        'beigu_laiks',
-        'apraksts',
-        'darbinieks_id',
-        'telpa_id',
-        'registracijas_beigu_datums',
-        'registracijas_beigu_laiks',
+        'pasakums_id',
+        'lietotajs_id',
+        'statuss',
+        'pieteikts_at',
+        'apmeklets_at',
     ];
 
-    public function images()
+    public function pasakums()
     {
-        return $this->hasMany(PasakumiImage::class, 'pasakumi_id', 'ID');
+        return $this->belongsTo(Pasakumi::class, 'pasakums_id', 'ID');
     }
 
-    public function telpa()
+    public function lietotajs()
     {
-        return $this->belongsTo(Telpa::class, 'telpa_id', 'ID');
-    }
-
-    public function darbinieks()
-    {
-        return $this->belongsTo(Lietotajs::class, 'darbinieks_id', 'ID');
-    }
-
-    public function pieteikumi()
-    {
-        return $this->hasMany(PasakumuPieteikums::class, 'pasakums_id', 'ID');
-    }
-
-    public function atsauksmes()
-    {
-        return $this->hasMany(PasakumuAtsauksme::class, 'pasakums_id', 'ID');
-    }
-
-    public function aktiviePieteikumi()
-    {
-        return $this->hasMany(PasakumuPieteikums::class, 'pasakums_id', 'ID')
-            ->whereIn('statuss', ['Pieteikts', 'Apmeklets']);
+        return $this->belongsTo(Lietotajs::class, 'lietotajs_id', 'ID');
     }
 }
