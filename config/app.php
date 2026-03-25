0bca555544df8db4cf63680a667663dc9ff00629
denisandrejev6-hue <denisandrejev6@gmail.com>
2026-03-25 08:49:11 +0000

    latvian

--- config/app.php
+++ config/app.php
@@ -78,11 +78,9 @@ return [
     |
     */
 
-    'locale' => env('APP_LOCALE', 'en'),
-
-    'fallback_locale' => env('APP_FALLBACK_LOCALE', 'en'),
-
-    'faker_locale' => env('APP_FAKER_LOCALE', 'en_US'),
+'locale' => 'lv',
+'fallback_locale' => 'lv',
+'faker_locale' => 'lv_LV',
 
     /*
     |--------------------------------------------------------------------------
